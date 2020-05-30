d98c667ebe38416b17ee1713b19d68a6aee0b64b
Housni Alami <alami.housni@gmail.com>
2020-05-30 11:39:38 +0200

    push exercise 3

--- exercises/1-loops/3-max.js
+++ exercises/1-loops/3-max.js
@@ -4,10 +4,16 @@ Given an array of numbers, create a function to return the maximum
 */
 
 function max(numbers){
-    //your code here
+    let biggestNum = -Infinity
+  for(let i=0; i < numbers.length; i++){
+      if(numbers[i]> biggestNum && numbers[i] % 2 === 0){
+          biggestNum = numbers[i]
+      }
+  }
+  return biggestNum
 }
 
-const numbersArray1 = [1,3,5,9];
+const numbersArray1 = [-4,-2];
 const numbersArray2 = [7,8,5,7,3,6];
 
 console.log("Maximum number in first array is " + max(numbersArray1));
--- exercises/1-loops/4-sum.js
+++ exercises/1-loops/4-sum.js
@@ -3,3 +3,12 @@ Sum
 If we sum all even numbers between 50 and 100 what is the result?
 Build a function, that calculates the sum of all even numbers between a start number and end number
 */
+let number = 50
+let sum = 0
+function sum(){
+    while (number<=100){
+sum =+ number
+number++
+    }
+}
+console.log(sum())
